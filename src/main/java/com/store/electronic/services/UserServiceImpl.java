757cdbfd4fe03681689d5b79c64e61eebe511314
Gaurab Sah <gaurab.sah.777@gmail.com>
2023-01-20 16:54:43 +0530

    delete user image when delete user and added logger

--- src/main/java/com/store/electronic/services/UserServiceImpl.java
+++ src/main/java/com/store/electronic/services/UserServiceImpl.java
@@ -5,13 +5,19 @@ import com.store.electronic.entities.User;
 import com.store.electronic.exceptions.ResourceNotFoundException;
 import com.store.electronic.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -25,6 +31,11 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private ModelMapper mapper;
 
+    @Value("${user.profile.image.path}")
+    private String imagePath;
+
+    private final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Override
     public UserDto createUser(UserDto userDto) {
 //        generate unique userId in string formate
@@ -36,6 +47,7 @@ public class UserServiceImpl implements UserService {
 
 //        entity -> dto
         UserDto newUser = entityToDto(savedUser);
+        logger.info("User created: {}", newUser);
         return newUser;
     }
 
@@ -51,29 +63,42 @@ public class UserServiceImpl implements UserService {
         user.setImageName(userDto.getImageName());
         User updatedUser = userRepository.save(user);
         UserDto updatedUserDto = entityToDto(updatedUser);
+        logger.info("User updated: {}", updatedUserDto);
         return updatedUserDto;
     }
 
     @Override
-    public List<UserDto> getAllUser(int pageNumber, int pageSize,String sortBy, String sortOrder) {
+    public List<UserDto> getAllUser(int pageNumber, int pageSize, String sortBy, String sortOrder) {
         Sort sort = (sortOrder.equalsIgnoreCase("desc")) ? (Sort.by(sortBy).descending()) : (Sort.by(sortBy).ascending());
-        Pageable pageable= PageRequest.of(pageNumber, pageSize,sort);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
         Page<User> page = userRepository.findAll(pageable);
         List<User> users = page.getContent();
         List<UserDto> userList = users.stream().map(user -> entityToDto(user)).collect(Collectors.toList());
+        logger.info("Users retrieved: {}", userList);
         return userList;
     }
 
     @Override
     public void deleteUser(String userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
+        String imageFullPath = imagePath + user.getImageName();
+        try {
+            Path path = Paths.get(imageFullPath);
+            Files.delete(path);
+            logger.info("File deleted: {}", imageFullPath);
+        } catch (Exception e) {
+            logger.info("No Such File Found");
+            e.printStackTrace();
+        }
         userRepository.delete(user);
+        logger.info("User Deleted Successfully");
     }
 
     @Override
     public UserDto getUserById(String userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
         UserDto userDto = entityToDto(user);
+        logger.info("User retrieved: {}", userDto);
         return userDto;
     }
 
@@ -81,6 +106,7 @@ public class UserServiceImpl implements UserService {
     public UserDto getUserByEmail(String email) {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
         UserDto newUserDto = entityToDto(user);
+        logger.info("User retrieved: {}", newUserDto);
         return newUserDto;
     }
 
@@ -88,6 +114,7 @@ public class UserServiceImpl implements UserService {
     public List<UserDto> searchUser(String keyword) {
         List<User> users = userRepository.findByNameContaining(keyword);
         List<UserDto> userList = users.stream().map(user -> entityToDto(user)).collect(Collectors.toList());
+        logger.info("Users retrieved: {}", userList);
         return userList;
     }
 
