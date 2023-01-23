c6b89cf946636cd2d5be60d7fdb24496dc2a1e03
Gaurab Sah <gaurab.sah.777@gmail.com>
2023-01-23 16:11:57 +0530

    added image upload

--- src/main/java/com/store/electronic/controllers/CategoryController.java
+++ src/main/java/com/store/electronic/controllers/CategoryController.java
@@ -1,14 +1,25 @@
 package com.store.electronic.controllers;
 
 import com.store.electronic.dtos.CategoryDto;
+import com.store.electronic.dtos.UserDto;
 import com.store.electronic.services.CategoryService;
+import com.store.electronic.services.FileService;
+import com.store.electronic.utils.ImageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -20,15 +31,21 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private FileService fileService;
+
+    @Value("${category.profile.image.path}")
+    private String imageUploadPath;
+
     @PostMapping("/create")
-    public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto) {
+    public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
         CategoryDto newCategoryDto = categoryService.saveCategory(categoryDto);
         logger.info("Category created: {}", newCategoryDto);
         return new ResponseEntity<>(newCategoryDto, HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{categoryId}")
-    public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable String categoryId) {
+    public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable String categoryId) {
         CategoryDto updatedCategoryDto = categoryService.updateCategory(categoryDto, categoryId);
         logger.info("Category updated: {}", updatedCategoryDto);
         return new ResponseEntity<>(updatedCategoryDto, HttpStatus.OK);
@@ -58,5 +75,33 @@ public class CategoryController {
         List<CategoryDto> categoryDtos = categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
         logger.info("Categories found: {}", categoryDtos);
         return new ResponseEntity<>(categoryDtos, HttpStatus.OK);
+    }
+
+    @PostMapping("/uploadCategoryImage/{categoryId}")
+    public ResponseEntity<ImageResponse> uploadUserImage(@RequestParam("categoryImage") MultipartFile image, @PathVariable String categoryId) throws IOException {
+        String imageName = fileService.uploadFile(image, imageUploadPath);
+
+        CategoryDto category = categoryService.getCategory(categoryId);
+        category.setCoverImage(imageName);
+        CategoryDto categoryDto = categoryService.updateCategory(category, categoryId);
+
+        ImageResponse imageResponse = ImageResponse.builder()
+                .imageName(imageName)
+                .success(true)
+                .status(HttpStatus.CREATED)
+                .message("Image uploaded successfully!!!")
+                .build();
+
+        return new ResponseEntity<>(imageResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getCategoryImage/{categoryId}")
+    public void downloadUserImage(@PathVariable String categoryId, HttpServletResponse response) throws IOException {
+        CategoryDto category = categoryService.getCategory(categoryId);
+        logger.info("Category Image Name: {}", category.getCoverImage());
+        InputStream resource = fileService.downloadFile(imageUploadPath, category.getCoverImage());
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
+        logger.info("Category Image Downloaded Successfully!!!");
     }
 }
